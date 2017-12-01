6a153e76c8242ce35bf0e1a5d4f6a6a1778c09f4
vince zipparro <zips55@sbcglobal.net>
2017-12-01 15:22:25 -0600

    functionality to add channels implemented.

---
+++ Smack/Services/SocketService.swift
@@ -0,0 +1,52 @@
+//
+//  SocketService.swift
+//  Smack
+//
+//  Created by vince zipparro on 12/1/17.
+//  Copyright © 2017 vince zipparro. All rights reserved.
+//
+
+import UIKit
+import SocketIO
+
+class SocketService: NSObject
+{
+    static let instance = SocketService()
+    
+    override init()
+    {
+        super.init()
+    }
+    
+    var socket : SocketIOClient = SocketIOClient(socketURL: URL(string: BASE_URL)!)
+    
+    func establishConnection()
+    {
+        socket.connect()
+    }
+    
+    func closeConnection()
+    {
+        socket.disconnect()
+    }
+    
+    func addChannel(channelName: String, channelDescription: String, completion : @escaping CompletionHandler)
+    {
+        socket.emit("newChannel", channelName, channelDescription)
+        completion(true)
+    }
+    
+    func getChannel(completion: @escaping CompletionHandler)
+    {
+        socket.on("channelCreated") { (dataArray, ack) in
+            guard let channelName = dataArray[0] as? String else {return}
+            guard let channelDesc = dataArray[1] as? String else {return}
+            guard let channelId = dataArray[2] as? String else {return}
+            
+            let newChannel = Channel(channelTitle: channelName, channelDescription: channelDesc, id: channelId)
+            MessageService.instance.channels.append(newChannel)
+            completion(true)
+        }
+    }
+    
+}
