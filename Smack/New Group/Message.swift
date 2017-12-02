f23b9430e0ead1c9d9a7aa9f60143570fe0f0fb6
vince zipparro <zips55@sbcglobal.net>
2017-12-02 13:49:26 -0600

    created model for messages, created function to retrieve messages for a given channel, functions added to check if there are more than one channel

---
+++ Smack/New Group/Message.swift
@@ -0,0 +1,21 @@
+//
+//  Message.swift
+//  Smack
+//
+//  Created by vince zipparro on 12/2/17.
+//  Copyright © 2017 vince zipparro. All rights reserved.
+//
+
+import Foundation
+
+struct Message
+{
+    public private(set) var message: String!
+    public private(set) var userName: String!
+    public private(set) var channelId: String!
+    public private(set) var userAvatar: String!
+    public private(set) var userAvatarColor: String!
+    public private(set) var id: String!
+    public private(set) var timeStamp: String!
+    
+}
